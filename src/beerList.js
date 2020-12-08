dfee22bee0dd645341ee4d2bf55414ae44ee0d1e
Kostya Polishko <kost927@gmail.com>
2020-12-08 15:14:19 +0200

    US002-003 - fixed review

--- src/beerList.js
+++ src/beerList.js
@@ -1,16 +1,17 @@
 import refs from "./refs.js";
+import { IMAGE_URL } from "./constants.js";
 
 export function getAllBeer(allBeer) {
-  const markup = allBeer.reduce((acc, beer) => {
-    if (beer.image_url === null) {
-      beer.image_url =
-        "https://p7.hiclipart.com/preview/1005/240/954/brewdog-craft-beer-for-the-people-brewdog-craft-beer-for-the-people-ale-punk-ipa-beer-thumbnail.jpg";
+    const markup = allBeer.reduce((acc, { image_url, name, description, abv }) => {
+    if (!image_url) {
+        image_url = IMAGE_URL;
     }
     acc += `<div class="beer">
-     <div class="beerImg"><img src="${beer.image_url}" width="150" hight="300" alt="beer picture" /></div>
+     <div class="beerImg"><img src="${image_url}" width="150" hight="300" alt="beer picture" /></div>
      <div class="beerText">
-     <div class="beerTitle">${beer.name}</div>
-     <div class="beerDescription">${beer.description}</div>
+     <div class="beerTitle">${name}</div>
+     <div class="beerABV">Alcohol (ABV) ${abv}%</div>
+     <div class="beerDescription">${description}</div>
      </div>
      </div>
      `;
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const IMAGE_URL = "https://p7.hiclipart.com/preview/1005/240/954/brewdog-craft-beer-for-the-people-brewdog-craft-beer-for-the-people-ale-punk-ipa-beer-thumbnail.jpg";
